f0504a6db258669b5cc8d69940680c4122f000cc
YuriGal <ygalanter@diligent.com>
2024-07-09 17:25:48 -0400

    Improvements

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,42 @@
-import { Bot } from '@skyware/bot';
+import { Bot, Post } from '@skyware/bot';
 import { SongCollection } from './songs.js';
 import * as dotenv from 'dotenv';
 import { CronJob } from 'cron';
-import { text } from 'stream/consumers';
 
-dotenv.config();
-
-const bot = new Bot();
-await bot.login({
-  identifier: process.env.BSKY_BOT_HANDLE!,
-  password: process.env.BSKY_BOT_PASSWORD!,
-});
-
-// initializing song collection
-const songCollection = new SongCollection();
-
-// replying to mentions
-bot.on('mention', async (mention) => {
+const reply = async (mention: Post) => {
   await mention.like();
-  console.log(`In reply to @${mention.author.handle}: ${mention.text}`);
+  console.log(`\nIn reply to @${mention.author.handle}: ${mention.text}\n`);
   await mention.reply({
     text: songCollection.song,
   });
-});
+};
+
+const createBot = async () => {
+  const bot = new Bot();
+  await bot.login({
+    identifier: process.env.BSKY_BOT_HANDLE!,
+    password: process.env.BSKY_BOT_PASSWORD!,
+  });
+
+  bot.on('mention', reply);
+
+  return bot;
+};
+
+dotenv.config();
+
+const songCollection = new SongCollection();
+
+let dynamicBot = await createBot();
 
 // posting a skeet every 2 hours
 const job = new CronJob('0 */2 * * *', async () => {
-  await bot.post({
+  dynamicBot = await createBot();
+  await dynamicBot.post({
     text: songCollection.song,
   });
 });
 
-console.log(`${new Date().toLocaleString()}: ****** Floyd Quoter Started *****\n`);
+console.log(`\n${new Date().toLocaleString()}: ****** Floyd Quoter Started *****\n`);
 
-// posting initial skeet, and starting the job
-await bot.post({
-  text: songCollection.song,
-});
 job.start();
