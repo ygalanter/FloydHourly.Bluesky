b70a8ab5e86261afb9a09b0f0dcc70e0d6354799
YuriGal <50464505+YuriGal@users.noreply.github.com>
2024-07-28 10:25:09 -0400

    updare

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,66 @@
-import { Bot } from '@skyware/bot';
+import { Bot, Post } from '@skyware/bot';
 import { SongCollection } from './songs.js';
 import * as dotenv from 'dotenv';
 import { CronJob } from 'cron';
-import { text } from 'stream/consumers';
 
-dotenv.config();
-
-const bot = new Bot();
-await bot.login({
-  identifier: process.env.BSKY_BOT_HANDLE!,
-  password: process.env.BSKY_BOT_PASSWORD!,
-});
-
-// initializing song collection
-const songCollection = new SongCollection();
-
-// replying to mentions
-bot.on('mention', async (mention) => {
+const reply = async (mention: Post) => {
   await mention.like();
-  console.log(`In reply to @${mention.author.handle}: ${mention.text}`);
+  console.log(`\nIn reply to @${mention.author.handle}: ${mention.text}\n`);
   await mention.reply({
     text: songCollection.song,
   });
-});
+};
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const createBot = async () => {
+  const bot = new Bot();
+
+  try {
+    await bot.login({
+      identifier: process.env.BSKY_BOT_HANDLE!,
+      password: process.env.BSKY_BOT_PASSWORD!,
+    });
+  } catch (error) {
+    console.error(`\n*** ${new Date().toLocaleString()}: Error logging in. Retrying in 1 minute.\n`);
+    await sleep(60000);
+    await bot.login({
+      identifier: process.env.BSKY_BOT_HANDLE!,
+      password: process.env.BSKY_BOT_PASSWORD!,
+    });
+  }
+
+  bot.on('mention', reply);
+  bot.on('error', (error) => {
+    console.error(`\n*** ${new Date().toLocaleString()}: Error: ${error}\n`);
+  });
+
+  return bot;
+};
+
+dotenv.config();
+
+const songCollection = new SongCollection();
+
+let dynamicBot = await createBot();
 
 // posting a skeet every 2 hours
 const job = new CronJob('0 */2 * * *', async () => {
-  await bot.post({
-    text: songCollection.song,
-  });
+  dynamicBot = await createBot();
+
+  try {
+    await dynamicBot.post({
+      text: songCollection.song,
+    });
+  } catch (error) {
+    console.error(`\n*** ${new Date().toLocaleString()}: Error posting. Retrying in 1 minute.\n`);
+    await sleep(60000);
+    await dynamicBot.post({
+      text: songCollection.song,
+    });
+  }
 });
 
-console.log(`${new Date().toLocaleString()}: ****** Floyd Quoter Started *****\n`);
+console.log(`\n${new Date().toLocaleString()}: ****** Floyd Quoter Started *****\n`);
 
-// posting initial skeet, and starting the job
-await bot.post({
-  text: songCollection.song,
-});
 job.start();
